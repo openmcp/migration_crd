17c61b4b8a87e6d3dde4063cde295e4bf51ed3fa
root <leelee3150@nanum.co.kr>
2020-04-23 14:51:39 +0900

    copy

--- pkg/controller/openmcpmigration/migration.go
+++ pkg/controller/openmcpmigration/migration.go
@@ -1,16 +1,1 @@
 package openmcpmigration
-
-// import (
-// 	nanumv1alpha1 "migration/pkg/apis/nanum/v1alpha1"
-// )
-
-// func ResourceMigration(migrationSpec nanumv1alpha1.OpenMCPMigrationSpec) {
-// 	targetConfig := migrationSpec.TargetCluster
-// 	sourceConfig := migrationSpec.SourceCluster
-// 	resourceName := migrationSpec.ResourceName
-
-// }
-
-// func PVMigration(migrationSpec nanumv1alpha1.OpenMCPMigrationSpec) {
-// 	pvType := migrationSpec.PVtype
-// }
